20b73456d96ce4c863ee20579bdf5647a2be16d8
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2022-11-18 12:07:15 +0900

    propsの試し

---
+++ components/Headline.tsx
@@ -0,0 +1,18 @@
+import styles from '../styles/Home.module.css'
+import React from "react"
+
+export default function Headline(props) {
+    console.log(props)
+    return (
+        <>
+            <h1 className={styles.title}>
+                {props.page} Page
+            </h1>
+
+            <p className={styles.description}>
+                Get started by editing{' '}
+                <code className={styles.code}>pages/{props.page}.tsx</code>
+            </p>
+        </>
+    )
+}
