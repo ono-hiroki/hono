e18ab405ec1a8d48cfdb3c8cfe355b9f2a27eb40
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2023-02-11 19:08:54 +0900

    useEffectの試し

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import {Footer} from "src/components/Footer"
 import {Main} from "src/components/Main"
 import {Header} from "src/components/Header"
-import React, {useCallback} from "react";
+import React, {useCallback, useEffect} from "react";
 import styled from 'styled-components';
 import Link from "next/link";
 
@@ -17,6 +17,16 @@ export default function Home() {
         console.log(e)
         alert(foo)
     }, []);
+
+    useEffect(() => {
+        console.log('useEffect')
+        document.body.style.backgroundColor = 'lightblue'
+
+        return () => {
+            console.log('useEffect return')
+            document.body.style.backgroundColor = 'lightgreen'
+        }
+    }, [])
 
     return (
         <Container>
