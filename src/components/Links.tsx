59a3bd07a99ea0d8eb24cf75de28aa41bb03abb7
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2023-02-12 17:26:17 +0900

    コンポーネント間のprops渡しの試し

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -1,33 +1,14 @@
 import styles from '../styles/Home.module.css'
-
-const ITEMS = [
-    {
-        href: 'https://nextjs.org/docs',
-        title: 'Documentation \u2192',
-        description: 'Find in-depth information about Next.js features and API.',
-    },
-    {
-        href: 'https://nextjs.org/learn',
-        title: 'Learn \u2192',
-        description: 'Learn about Next.js in an interactive course with quizzes!',
-    },
-    {
-        href: 'https://github.com/vercel/next.js/tree/canary/examples',
-        title: 'Examples \u2192',
-        description: 'Discover and deploy boilerplate example Next.js projects.',
-    },
-    {
-        href: 'https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app',
-        title: 'Deploy \u2192',
-        description: 'Instantly deploy your Next.js site to a public URL with Vercel.',
-    },
-]
+import {useCallback, useState} from "react";
 
 
-export function Links() {
+export function Links(props: any) {
+const {items} = props
+
     return (
                 <div className={styles.grid}>
-                    {ITEMS.map(item => {
+                    {/*@ts-ignore*/}
+                    {items.map(item => {
                         return (
                             <a key={item.href} href={item.href} className={styles.card}>
                                 <h2>{item.title}</h2>
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,21 +1,53 @@
 import styles from '../styles/Home.module.css'
 import {Links} from "./Links";
 import {Headline} from "./Headline";
-import React from "react";
+import React, {useCallback, useState} from "react";
 
 type Props = {
     page: string
 }
 
+
+const ITEMS = [
+    {
+        href: 'https://nextjs.org/docs',
+        title: 'Documentation \u2192',
+        description: 'Find in-depth information about Next.js features and API.',
+    },
+    {
+        href: 'https://nextjs.org/learn',
+        title: 'Learn \u2192',
+        description: 'Learn about Next.js in an interactive course with quizzes!',
+    },
+    {
+        href: 'https://github.com/vercel/next.js/tree/canary/examples',
+        title: 'Examples \u2192',
+        description: 'Discover and deploy boilerplate example Next.js projects.',
+    },
+    {
+        href: 'https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app',
+        title: 'Deploy \u2192',
+        description: 'Instantly deploy your Next.js site to a public URL with Vercel.',
+    },
+]
+
+
 export function Main(props: Props) {
+    const [items, setItems] = useState(ITEMS)
+    const handleReduce = useCallback(() => {
+        setItems(items => items.slice(0, items.length - 1))
+    }, [])
+
     return (
             <main className={styles.main}>
+                <button onClick={handleReduce}>減らす</button>
                 <Headline
                     page={props.page}
                 >
-                    <code className={styles.code}>pages/{props.page}.tsx</code>
+                    <code className={styles.code}>pages/{props.page}.tsx</code><br/>
+                   アイテムの数は{items.length}です
                 </Headline>
-                <Links />
+                <Links items={items} />
             </main>
     )
 }
