5ba89d0c7851a172f534dc39aced0d15937e983b
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2023-02-19 21:09:17 +0900

    コンパイルエラーの修正

--- src/pages/EdgeWithButton.tsx
+++ src/pages/EdgeWithButton.tsx
@@ -11,7 +11,7 @@ import ReactFlow, {
 } from 'reactflow';
 import 'reactflow/dist/style.css';
 
-import ButtonEdge from '../components/EdgeWithButton/ButtonEdge';
+import ButtonEdge from 'src/components/EdgewithButton/ButtonEdge.js';
 import {useGetWindowSize} from "../hooks/useGetWindowSize";
 
 const initialNodes = [
