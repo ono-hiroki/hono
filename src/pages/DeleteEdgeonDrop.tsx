3bac538c1eb9813150bd0ccab6f236200a2ec6cd
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2023-02-19 14:20:22 +0900

    とりあえず型

--- src/pages/DeleteEdgeonDrop.tsx
+++ src/pages/DeleteEdgeonDrop.tsx
@@ -1,5 +1,5 @@
 import React, {useCallback, useRef} from 'react';
-import ReactFlow, {useNodesState, useEdgesState, Controls, updateEdge, addEdge} from 'reactflow';
+import ReactFlow, {useNodesState, useEdgesState, Controls, updateEdge, addEdge, Connection, Edge} from 'reactflow';
 import 'reactflow/dist/style.css';
 import {useGetWindowSize} from "../hooks/useGetWindowSize";
 
@@ -29,18 +29,18 @@ const DeleteEdgeDrop = () => {
     const edgeUpdateSuccessful = useRef(true);
     const [nodes, , onNodesChange] = useNodesState(initialNodes);
     const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-    const onConnect = useCallback((params) => setEdges((els) => addEdge(params, els)), []);
+    const onConnect = useCallback((params: Edge<any> | Connection) => setEdges((els) => addEdge(params, els)), []);
 
     const onEdgeUpdateStart = useCallback(() => {
         edgeUpdateSuccessful.current = false;
     }, []);
 
-    const onEdgeUpdate = useCallback((oldEdge, newConnection) => {
+    const onEdgeUpdate = useCallback((oldEdge: Edge<any>, newConnection: Connection) => {
         edgeUpdateSuccessful.current = true;
         setEdges((els) => updateEdge(oldEdge, newConnection, els));
     }, []);
 
-    const onEdgeUpdateEnd = useCallback((_, edge) => {
+    const onEdgeUpdateEnd = useCallback((_: any, edge: { id: string; }) => {
         if (!edgeUpdateSuccessful.current) {
             setEdges((eds) => eds.filter((e) => e.id !== edge.id));
         }
