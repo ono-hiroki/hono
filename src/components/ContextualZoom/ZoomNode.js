fc736122b8c1d25c51986d4c2c95fa80cb49ed94
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2023-02-19 20:19:58 +0900

    Contextual Zoom の試し

---
+++ src/components/ContextualZoom/ZoomNode.js
@@ -0,0 +1,25 @@
+import React, { memo } from 'react';
+import { Handle, useStore, Position } from 'reactflow';
+
+const Placeholder = () => (
+    <div className="placeholder">
+        <div />
+        <div />
+        <div />
+    </div>
+);
+
+const zoomSelector = (s) => s.transform[2];
+
+export default memo(({ data }) => {
+    const zoom = useStore(zoomSelector);
+    const showContent = zoom >= 1.5;
+
+    return (
+        <>
+            <Handle type="target" position={Position.Left} />
+            {showContent ? data.content : <Placeholder />}
+            <Handle type="source" position={Position.Right} />
+        </>
+    );
+});
