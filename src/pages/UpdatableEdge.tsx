fcea08e7f1ae2b91f8e303ba17a2b11a70c53baa
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2023-02-20 15:37:55 +0900

    UpdatableEdge のコメント

--- src/pages/UpdatableEdge.tsx
+++ src/pages/UpdatableEdge.tsx
@@ -22,13 +22,13 @@ const initialNodes = [
     },
 ];
 
-const initialEdges = [{id: 'e1-2', source: '1', target: '2', label: 'updatable edge'}];
+const initialEdges = [{id: 'e1-2', source: '1', target: '2', label: 'これを選択してから、エッジを移動するとエッジの更新(移動)ができる' }];
 
 const UpdatableEdge = () => {
     const {width: windowWidth, height: windowHeight} = useGetWindowSize();
     const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
     const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-    // gets called after end of edge gets dragged to another source or target
+    // 他のソースまたはターゲットにエッジの終端をドラッグした後に呼び出されます
     const onEdgeUpdate = useCallback(
         (oldEdge: Edge<any>, newConnection: Connection) => setEdges((els) => updateEdge(oldEdge, newConnection, els)),
         []
