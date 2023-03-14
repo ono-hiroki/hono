ad82b88a6bba1aef69a1033fb725d1655f8df5db
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2023-03-14 09:09:58 +0900

    ugomemo8-2.tsx

---
+++ src/pages/ugomemo8-2.tsx
@@ -0,0 +1,93 @@
+import Head from 'next/head';
+import {useEffect, useRef} from "react";
+import {useInView} from "react-intersection-observer";
+import {css, keyframes} from "@emotion/react";
+
+const p = css`
+margin: 20px;
+font-size: 20px;
+overflow: hidden;
+`
+
+const leftAnime = css`
+    opacity: 0;
+`
+
+const slideIn = css`
+    overflow: hidden;
+    display: inline-block;
+`
+
+const slideIn_inner = css`
+    display: inline-block;
+`
+
+const slideTextX100 = keyframes`
+  from {
+    transform: translateX(-100%); /*要素を左の枠外に移動*/
+        opacity: 0;
+  }
+
+  to {
+    transform: translateX(0);/*要素を元の位置に移動*/
+    opacity: 1;
+  }
+`
+
+const slideAnimeLeftRight = css`
+    animation-name:${slideTextX100};
+    animation-duration:0.8s;
+    animation-fill-mode:forwards;
+    opacity: 0;
+`
+
+const slideTextX_100 = keyframes`
+  from {
+    transform: translateX(100%);/*要素を右の枠外に移動*/
+    opacity: 0;
+  }
+
+  to {
+    transform: translateX(0);/*要素を元の位置に移動*/
+    opacity: 1;
+  }
+`
+
+const slideAnimeRightLeft = css`
+    animation-name:${slideTextX_100};
+    animation-duration:0.8s;
+    animation-fill-mode:forwards;
+    opacity: 0;
+`
+
+
+const Ugomemo81 = (props: any) => {
+    const useSlideAnimeLeftRight = useInView({
+            threshold: 0,
+        });
+
+    return (
+        <>
+            <Head>
+                <title>ugomemo8-2</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css"/>
+            </Head>
+
+            <p ref={useSlideAnimeLeftRight.ref} css={p}>
+	            <span css={[slideIn, leftAnime, useSlideAnimeLeftRight.inView && slideAnimeLeftRight]}>
+	                <span css={[slideIn_inner, useSlideAnimeLeftRight.inView && slideAnimeRightLeft]}>
+                        左から右へテキストが流れます。左から右へテキストが流れます。
+	                </span>
+	            </span>
+            </p>
+
+
+            <br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓<br/>↓
+            <style jsx global>{Style}</style>
+        </>
+    )
+}
+export default Ugomemo81;
+
+const Style = `
+`
