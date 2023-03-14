88442727d8aa4f5d4d3f573ea10fcbf4b6d13a27
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2023-03-14 19:38:32 +0900

    背景の動き

--- src/components/WebSite01/Splash.tsx
+++ src/components/WebSite01/Splash.tsx
@@ -2,27 +2,6 @@ import React, {useEffect, useRef, useState} from "react";
 import {useInView} from "react-intersection-observer";
 import {css, keyframes} from "@emotion/react";
 
-const splash = css`
-position: fixed;
-width: 100%;
-height: 100%;
-background:#e2a2b1;
-z-index: 9999999;
-text-align:center;
-color:#fff;
-`
-
-const splashLogo = css`
-    font-size:1.8rem;
-    font-weight: normal;
-    white-space: nowrap;
-font-family:'Parisienne', cursive;
-position: absolute;
-top: 50%;
-left: 50%;
-transform: translate(-50%, -50%);
-`
-
 const leftAnime = css`
     opacity: 0;
 `
@@ -68,45 +47,119 @@ const slideAnimeRightLeft = css`
     opacity: 0;
 `
 
+const splash = css`
+position: fixed;
+width: 100%;
+height: 100%;
+background:#e2a2b1;
+z-index: 9999999;
+text-align:center;
+color:#fff;
+`
+
+const splashLogo = css`
+    font-size:1.8rem;
+    font-weight: normal;
+    white-space: nowrap;
+    font-family:'Parisienne', cursive;
+position: absolute;
+top: 50%;
+left: 50%;
+transform: translate(-50%, -50%);
+`
+
+const hiddenSplash = css`
+    display: none;
+`
+
+const PageAnime = keyframes`
+  0% {
+    transform-origin:left;
+    transform:scaleX(1);
+  }
+  50% {
+    transform-origin:right;
+  }
+  100% {
+    transform-origin:right;
+    transform:scaleX(0);
+  }
+`
+
+const splashbg1Appear = css`
+    animation-name:${PageAnime};
+    animation-duration:1.2s;
+    animation-timing-function:ease-in-out;
+    animation-fill-mode:forwards;
+    content: "";
+    position:fixed;
+    z-index: 999;
+    width: 100%;
+    height: 100vh;
+    top: 0;
+    left:50%;
+    transform: scaleX(1);
+    background-color:#e2a2b1;/*伸びる背景色の設定*/
+`
+
+const PageAnimeSecond = keyframes`
+  0% {
+    transform-origin:right;
+    transform:scaleX(1);
+  }
+
+  50% {
+    transform-origin:left;
+  }
+  100% {
+    transform-origin:left;
+    transform:scaleX(0);
+  }
+`
+
+const splashbg2Appear = css`
+    animation-name: ${PageAnimeSecond};
+    animation-duration:1.2s;
+    animation-timing-function:ease-in-out;
+    animation-fill-mode:forwards;
+    content: "";
+    position:fixed;
+    z-index: 999;
+    width: 100%;
+    height: 100vh;
+    top: 0;
+    right:50%;
+    transform: scaleX(1);
+    background-color:#e2a2b1;/*伸びる背景色の設定*/
+`
+
 const SplashTag = (props: { text: string; }) => {
     const [isTimePassed, setIsTimePassed] = useState(false);
-
     useEffect(() => {
         setTimeout(() => {
             setIsTimePassed(true)
-        }, 1400);
+        }, 1500);
     });
 
-    const [ref,inView] = useInView({
+    const [ref, inView] = useInView({
         threshold: 0,
     });
 
-
-
     return (
         <>
-            <div id="splash" css={splash} ref={ref}>
-                <div id="splash-logo" css={splashLogo}>
+            <div id="splash" css={[splash, isTimePassed && hiddenSplash]} ref={ref}>
+                <div id="splash-logo" css={[splashLogo]}>
                     <p>
                         <span className="slide-in slideAnimeLeftRight" css={[slideIn, leftAnime, slideAnimeLeftRight]}>
-                            <span className="slide-in_inner slideAnimeRightLeft" css={[slideIn_inner, slideAnimeRightLeft]}>Beautiful Days {inView}</span>
+                            <span className="slide-in_inner slideAnimeRightLeft"
+                                  css={[slideIn_inner, slideAnimeRightLeft]}>Beautiful Days {inView}</span>
                         </span>
                     </p>
                 </div>
             </div>
 
-            <div className="splashbg1"/>
-            <div className="splashbg2"/>
-
-            {/*<Splash className={fadeProp.fade}>*/}
-            {/*    <SplashLogo id='splash-logo'>*/}
-            {/*        <p>*/}
-            {/*            <SplashLogoPinSpan ref={leftAnime}>*/}
-            {/*                <SplashLogoPinSpanInner ref={leftAnimeInner}>Beautiful Days</SplashLogoPinSpanInner>*/}
-            {/*            </SplashLogoPinSpan>*/}
-            {/*        </p>*/}
-            {/*    </SplashLogo>*/}
-            {/*</Splash>*/}
+            <div className="splashbg1" css={[isTimePassed && splashbg1Appear]}/>
+            <div className="splashbg2" css={[isTimePassed && splashbg2Appear]}/>
         </>
     )
 }
