6667a5521dc3bbcec6688942edf52f84fdcd7d3f
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2023-04-23 10:34:48 +0900

    reactflowのページの追加

---
+++ src/components/Domain/ReactFlowLists.ts
@@ -0,0 +1,10 @@
+export const ReactFlowLists = [
+    {
+        href: 'https://github.com/ono-hiroki',
+        title: 'github \u2192',
+        imageSrc: '/myLists/img.png',
+        description: 'githubのアカウント。',
+        tags: ['github'],
+    },
+
+];
---
+++ src/pages/reactflow/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {TailWindHeader} from "../../components/TailWindHeader";
+import {Card} from "../../components/reactflow/Card";
+
+const Index = (props: any) => {
+
+    return (
+        <>
+            <TailWindHeader/>
+            <Card />
+        </>
+    )
+}
+
+export default Index;
