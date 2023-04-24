d55c2f2a256b992bd6c9eb3dcc98692dbf074803
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2023-04-24 10:19:31 +0900

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,6 @@ const Index = (props: any) => {
     return (
         <>
             <TailWindHeader/>
-            {/*about me の作成*/}
             <div className="flex flex-col items-center justify-center min-h-screen py-2">
                 <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
                     <h1 className="text-6xl font-bold">
