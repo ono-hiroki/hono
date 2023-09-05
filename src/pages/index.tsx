dadad3142c4729a1074cd5b56506850285877916
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2023-09-05 13:05:33 +0900

    自作パッケージの試し

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import {TailWindHeader} from "../components/TailWindHeader";
 import {Card} from "../components/Card";
+import {Greeter} from "practice-create-npm-package";
+const greeter = new Greeter();
 
 const Index = (props: any) => {
-
+    greeter.greet('hoge');
     return (
         <>
+
+            aa
             <TailWindHeader/>
             <Card/>
         </>
