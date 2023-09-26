0f365f245468d6266ad7f59fedaddc1a942306b1
ono-hiroki <thisismymailaddress13771377@docomo.ne.jp>
2023-09-26 21:08:02 +0900

    自作パッケージの試し

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,20 @@
 import '../styles/globals.css'
 import '../styles/sass/top.scss'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
 import {useCounter} from "../hooks/useCounter";
 import {useInputArray} from "../hooks/useInputArray";
+import {LayerProvider} from "hono-my-npm-package";
 
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  const counter = useCounter()
-  const inputArray = useInputArray()
-  // useBgColor()
+export default function MyApp({Component, pageProps}: AppProps) {
+    const counter = useCounter()
+    const inputArray = useInputArray()
+    // useBgColor()
 
-  return <Component {...pageProps} foo={'bar'} {...counter} {...inputArray} />
+    return (
+        <LayerProvider>
+            <Component {...pageProps} foo={'bar'} {...counter} {...inputArray} />
+        </LayerProvider>
+    )
+
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {TailWindHeader} from "../components/TailWindHeader";
 import {Card} from "../components/Card";
 import {Greeter} from "practice-create-npm-package";
+import {Hello, Layer, useLayerParams} from "hono-my-npm-package";
 const greeter = new Greeter();
 
 const Index = (props: any) => {
     greeter.greet('hoge');
+    Hello();
+    const [getInputRegExp, setInputRegExp] = useLayerParams('hoge', ["Float", "Integer"]);
+    useEffect(() => {
+        setInputRegExp('hoge', 'Float');
+        console.log(getInputRegExp());
+    }, []);
+
+
+
+
     return (
         <>
-
+            <Layer condition={false}>
+                <div>hoge</div>
+            </Layer>
             aa
             <TailWindHeader/>
             <Card/>
